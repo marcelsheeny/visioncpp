9e3d66a424f2414468b5383dcbd87be4c4782193
Marcel Sheeny <marcelsheeny@gmail.com>
2018-05-04 18:05:28 +0100

    modifying bridge pattern

--- include/utils/io_backend.hpp
+++ include/utils/io_backend.hpp
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <string>
 
+#ifdef USE_CIMG
+#include "CImg.h"
+using namespace cimg_library;
+#else
+#include <opencv2/opencv.hpp>
+#endif
+
 /* Implemented interface. */
 class AbstractInterface {
  public:
@@ -9,58 +16,110 @@ class AbstractInterface {
 };
 
 /* Interface for internal implementation that Bridge uses. */
-class IOHandler {
+class ImplementationHandler {
  public:
-  virtual void InitMemory() = 0;
-  virtual void save() = 0;
-  virtual void display() = 0;
+  virtual void read(char* filename) = 0;
+  virtual void save(char* output_file) = 0;
+  virtual void display(char* output_file) = 0;
+
+ private:
+  std::shared_ptr<T> output_ptr;
+  std::shared_ptr<T> input_ptr;
+#ifdef USE_CIMG
+  CImg<unsigned char> outputImage;
+#else
+  cv::Mat outputImage;
+#endif
 };
 
 /* The Bridge */
 class Bridge : public AbstractInterface {
  protected:
-  IOHandler* implementation;
+  ImplementationHandler* implementation;
 
  public:
-  Bridge(IOHandler* backend) { implementation = backend; }
+  Bridge(ImplementationHandler* backend) { implementation = backend; }
 };
 
 /* Different special cases of the interface. */
 
-class UseCase1 : public Bridge {
+class UseCase : public Bridge {
  public:
-  UseCase1(IOHandler* backend) : Bridge(backend) {}
+  UseCase(ImplementationHandler* backend) : Bridge(backend) {}
 
-  void someFunctionality() {
-    std::cout << "UseCase1 on ";
-    implementation->anotherFunctionality();
-  }
+  void read(char* filename) { implementation->read(filename); }
+  void save(char* filename) { implementation->save(filename); }
+  void display(char* filename) { implementation->display(filename); }
 };
 
 /* Different background implementations. */
-
-class opencv_backend : public IOHandler {
+template <int COLS, int ROWS, int CHANNELS, typename T>
+class opencv_backend : public ImplementationHandler {
  public:
-  void anotherFunctionality() { std::cout << "opencv :-!" << std::endl; }
+  void read(const char* filename) {
+    output_ptr =
+        std::shared_ptr<T>(new unsigned char[COLS * ROWS * CHANNELS],
+                           [](unsigned char* dataMem) { delete[] dataMem; });
+
+    printf("Using OpenCV\n");
+
+    cv::Mat input = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::resize(input, input, cv::Size(COLS, ROWS));
+    input_ptr = std::shared_ptr<T>(input.data);
+    outputImage = cv::Mat(ROWS, COLS, CV_8UC(CHANNELS), output_ptr.get());
+  }
+
+  void save(char* output_file) { cv::imwrite(output_file, outputImage); }
+
+  void display(char* output_file) {
+    cv::imshow(output_file, outputImage);
+    cv::waitKey(0);
+  }
 };
 
-class cimg_backend : public IOHandler {
+class cimg_backend : public ImplementationHandler {
  public:
-  void anotherFunctionality() { std::cout << "cimg! :-)" << std::endl; }
+  void read(const char* filename) {
+    output_ptr =
+        std::shared_ptr<T>(new unsigned char[COLS * ROWS * CHANNELS],
+                           [](unsigned char* dataMem) { delete[] dataMem; });
+
+    printf("Using OpenCV\n");
+
+    cv::Mat input = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::resize(input, input, cv::Size(COLS, ROWS));
+    input_ptr = std::shared_ptr<T>(input.data);
+    outputImage = cv::Mat(ROWS, COLS, CV_8UC(CHANNELS), output_ptr.get());
+  }
+
+  void save(char* output_file) { outputImage.save(output_file); }
+
+  void display(char* output_file) { outputImage.display(output_file); }
 };
 
-int main() {
-  AbstractInterface* useCase = 0;
+class IOHandler {
+  IOHandler() {
 #ifdef USE_CIMG
-  ImplementationInterface* backend = new opencv_backend;
+    ImplementationInterface* backend = new opencv_backend;
 #elif USE_OPENCV
-  ImplementationInterface* backend = new cimg_backend;
+    ImplementationInterface* backend = new cimg_backend;
 #else
-  ImplementationInterface* backend = new opencv_backend;
-
-  /* First case */
-  useCase = new UseCase1(backend);
-  useCase->someFunctionality();
-
-  return 0;
+    ImplementationInterface* backend = new opencv_backend;
 }
+void read(char* filename){
+  useCase->read(filename);
+}
+
+void save(char* filename){
+  useCase->save(filename);
+}
+
+void display(char* filename){
+  useCase->display(filename);
+}
+
+private:
+  UseCase useCase = new UseCase(backend);
+  ImplementationInterface* backend = 0;
+  AbstractInterface* useCase = 0;
+};
